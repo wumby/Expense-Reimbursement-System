cfa041b5dd001c33b7c18ceb902be4fea50a5389
Jack Ziegler <64660004+wumby@users.noreply.github.com>
2022-03-19 15:19:33 -0500

    init

--- project1/backend/src/main/java/com/revature/main/Driver.java
+++ project1/backend/src/main/java/com/revature/main/Driver.java
@@ -4,18 +4,14 @@ import com.revature.controller.AssignmentController;
 import com.revature.controller.AuthenticationController;
 import com.revature.controller.Controller;
 import com.revature.controller.ExceptionController;
-import com.revature.dao.UserDao;
-import com.revature.utility.ConnectionUtility;
 import io.javalin.Javalin;
-
-import java.sql.SQLException;
 
 public class Driver {
 
     public static void main(String[] args) {
         Javalin app = Javalin.create((config) -> {
             config.enableCorsForAllOrigins();
-//            config.enableCorsForOrigin("http://localhost:8081");
+            //config.enableCorsForOrigin("http://localhost:8081");
         });
 
         map(app, new AuthenticationController(), new ExceptionController(), new AssignmentController());
