553575069b4bfd7fb5e0c4879984003724ce8c0e
Jack Ziegler <64660004+wumby@users.noreply.github.com>
2022-03-22 16:44:31 -0500

    added logging

--- project1/backend/src/main/java/com/revature/main/Driver.java
+++ project1/backend/src/main/java/com/revature/main/Driver.java
@@ -9,14 +9,20 @@ import io.javalin.Javalin;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Driver {
-
+    public static Logger logger = LoggerFactory.getLogger("mylogfile.log");
     public static void main(String[] args) {
         Javalin app = Javalin.create((config) -> {
             config.enableCorsForAllOrigins();
             //config.enableCorsForOrigin("http://localhost:8081");
         });
+
+        app.before((ctx -> {
+            logger.info(ctx.method() + " request received for " + ctx.path());
+        }));
 
         map(app, new AuthenticationController(), new ExceptionController(), new ReimbursementController());
 
